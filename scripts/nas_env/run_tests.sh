d930d648caa2b06203867a9c127ca8f076d5670e
Jorge Gomez <gomerudo@gmail.com>
2019-04-23 15:21:13 +0200

    run_tests for nas-env

---
+++ scripts/nas_env/run_tests.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+CURRENT_SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+SET_GLOBALVARS_PATH=${CURRENT_SCRIPT_DIR}/../setup/set_globalvars.sh
+
+# Load the global vars
+source ${SET_GLOBALVARS_PATH}
+# Load the conda script
+source ${SETUP_CONDA_PATH}
+# Source the environment
+source activate ${VENV_NAME}
+
+pushd ${NASENV_PATH}
+python -m test -v
+popd
