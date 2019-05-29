18454f4d67403c9c49ac9db9e9098799bd927f7a
Jorge Gomez <gomerudo@gmail.com>
2019-05-29 17:06:28 +0200

    Update NAS_DMRL script

--- scripts/experiments/run_nas_dmrl.sh
+++ scripts/experiments/run_nas_dmrl.sh
@@ -1,8 +1,92 @@
 #!/bin/bash
 
-# Obtain the PID since the beginning, to help debugging.
+################################################################################
+################################## FUNCTIONS  ##################################
+################################################################################
+
+usage() {
+    echo "Usage:"
+    echo ""
+    echo "     run_nas_dmrl.sh -c CONFIG_FILE [-m PREWARM_MODEL]"
+    echo ""
+}
+
+parse_var() {
+    echo `grep  -E "${1}\s*=\s*.+" ${INI_FILE} | sed 's/\([[:alpha:]]*[[:space:]]*=[[:space:]]*\)\(.*\)$/\2/'`
+}
+parse_ini_file() {
+    RL_ALGORITHM="$(parse_var Algorithm)"
+    RL_ENVIRONMENT="$(parse_var Environment)"
+    RL_NETWORK="$(parse_var Network)"
+    FINAL_MODEL_NAME="$(parse_var FinalModelName)"
+    N_TASKS="$(parse_var NTasks)"
+    N_STEPS="$(parse_var NSteps)"
+    N_TIMESTEPS="$(parse_var NumTimesteps)"
+    N_TRIALS="$(parse_var NTrials)"
+    GPU_MONITOR_SECONDS="$(parse_var GPUMonitorSec)"
+    SLEEP_TIME_SECONDS="$(parse_var SleepTimeSec)"
+}
+
+################################################################################
+################################ PARSE OPTIONS  ################################
+################################################################################
+
+while getopts ":c:m:" opt; do
+  case ${opt} in
+    c )
+        INI_FILE=$OPTARG
+        ;;
+    m )
+        WARMUP_MODEL=$OPTARG
+        ;;
+
+    \? )
+        usage
+        exit 1
+        ;;
+    : )
+        usage
+        exit 1
+        ;;
+  esac
+done
+
+################################################################################
+########################### VALIDATE MANDATORY ARGS  ###########################
+################################################################################
+
+if [ -z ${INI_FILE} ]; then
+    usage
+    exit 1
+fi
+
+################################################################################
+################################ LOG ARGUMENTS  ################################
+################################################################################
+
+echo "Experiment will use the configuration in file ${INI_FILE}"
+export NAS_DMRL_CONFIG_FILE=${INI_FILE}
+
+if [ ! -z ${WARMUP_MODEL} ]; then
+    echo "Experiment will use model ${WARMUP_MODEL} as a pre-warm model."
+fi
+
+################################################################################
+################################ GET MASTER PID  ################################
+################################################################################
+
 MASTER_PID=$$
-echo "Master process' PID: ${MASTER_PID}"
+echo "Master process PID: ${MASTER_PID}"
+
+################################################################################
+############################### PARSE CONFIG.INI ###############################
+################################################################################
+
+parse_ini_file
+
+################################################################################
+############################# SET GLOBAL VARIABLES #############################
+################################################################################
 
 # Obtain the script's current directory
 CURRENT_SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
@@ -11,58 +95,108 @@ CURRENT_SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && 
 SET_GLOBALVARS_PATH=${CURRENT_SCRIPT_DIR}/../setup/set_globalvars.sh
 source ${SET_GLOBALVARS_PATH}
 
-# Activate the environment
+# The start timestamp
+START_TIMESTAMP=`date +%Y%m%d%H%M%S`
+
+################################################################################
+########################## ACTIVATE CONDA ENVIRONMENT ##########################
+################################################################################
+
 source ${SETUP_CONDA_PATH}
 echo "Activating conda environment ${VENV_NAME}"
 conda activate ${VENV_NAME}
 
-# Obtain the current timestep, so that we can create isolated logs
-current_date=`date +%Y%m%d%H%M%S`
-LOGDIR="${WORKSPACE}/results/opeanai-${current_date}"
+################################################################################
+########################## MAKE EXPERIMENTS DIRECTORY ##########################
+################################################################################
+EXPERIMENT_DIR="${WORKSPACE}/results/experiment-${START_TIMESTAMP}"
 
-# Export the OPENAI_LOGDIR as needed by openai-baselines project
-export OPENAI_LOGDIR=${LOGDIR}
-
-# Define the place where we will store the resulting model
-SAVE_DIR=${OPENAI_LOGDIR}/models
-
-if [ ! -d ${SAVE_DIR} ]; then
-    mkdir -p ${SAVE_DIR}
+if [ ! -d ${EXPERIMENT_DIR} ]; then
+    echo "Making directory for the experiment"
+    mkdir -p ${EXPERIMENT_DIR}
 fi
 
-# Run the baseline
+################################################################################
+############################## RUN THE EXPRIMENTS ##############################
+################################################################################
+
+# Copy the configuration file so that we can know what we config we used
+cp ${INI_FILE} ${EXPERIMENT_DIR}/.
+
+echo "A total of ${N_TRIALS} trials will be tried"
+
+# Let the nvidia monitoring running in the background for as long as the process runs
+echo "Running GPU monitoring tool in the background. It will log every ${GPU_MONITOR_SECONDS} seconds"
+nvidia-smi \
+--query-gpu=timestamp,temperature.gpu,utilization.gpu,utilization.memory,memory.total,memory.free,memory.used \
+--format=csv \
+-l ${GPU_MONITOR_SECONDS} > ${EXPERIMENT_DIR}/smi-${START_TIMESTAMP}.csv 2>&1 &
+
 pushd ${OPENAI_BASELINES_PATH}
 
 if [ ! -d workspace ]; then
     mkdir workspace
 fi
 
-git checkout experiments
+for trial in $(seq 1  1 ${N_TRIALS}); do
+    echo "Starting trial ${trial}"
 
-# Let the nvidia monitoring running in the background for as long as the process runs
-echo "Running GPU monitoring tool in the background"
-nvidia-smi \
---query-gpu=timestamp,temperature.gpu,utilization.gpu,utilization.memory,memory.total,memory.free,memory.used \
---format=csv \
--l 5 > ${WORKSPACE}/runs/smi-${current_date}.csv 2>&1 &
+    # We sleep for few seconds to let the memory release and the environment stabilize
+    # Each trial will have its one timestamp to isolate the subexperiments
+    TRIAL_TIMESTAMP=`date +%Y%m%d%H%M%S`
 
-time python -m baselines.run \
---alg=meta_a2c \
---env=NAS_cifar10-v1 \
---network=meta_lstm \
---save_path=${SAVE_DIR}/meta_a2c_final.model \
---n_tasks=1 \
---nsteps=5 \
---num_timesteps=1e3
-#--load_path=/home/TUE/20175601/workspace/results/opeanai-20190528182301/models/meta_a2c_tmp-1.mdl
+    # We obtain the last OPENAI_LOGDIR in the environmet, to use it as the
+    # warmup model.
+    LAST_OPENAI_LOGDIR="${OPENAI_LOGDIR}"
 
+    # Export the OPENAI_LOGDIR as needed by openai-baselines project
+    LOGDIR="${EXPERIMENT_DIR}/opeanai-${TRIAL_TIMESTAMP}"
+    export OPENAI_LOGDIR=${LOGDIR}
+
+    # Define the place where we will store the resulting model
+    SAVE_DIR=${OPENAI_LOGDIR}/models
+
+    if [ ! -d ${SAVE_DIR} ]; then
+        mkdir -p ${SAVE_DIR}
+    fi
+
+    command="time python -m baselines.run \
+--alg=${RL_ALGORITHM} \
+--env=${RL_ENVIRONMENT} \
+--network=${RL_NETWORK} \
+--save_path=${SAVE_DIR}/${FINAL_MODEL_NAME} \
+--n_tasks=${N_TASKS} \
+--nsteps=${N_STEPS} \
+--num_timesteps=${N_TIMESTEPS}"
+
+    if [ ${trial} -eq 1 ]; then
+        if [ ! -z ${WARMUP_MODEL} ]; then
+            command="${command} --load_path=${WARMUP_MODEL}"
+        fi
+    else
+        if [ ! -z ${LAST_OPENAI_LOGDIR} ]; then
+            command="${command} --load_path=${LAST_OPENAI_LOGDIR}"
+        fi
+    fi
+
+    echo "Command to execute is: ${command}"
+    ${command}
+    
+    # Always wait after the command has been executed
+    sleep ${SLEEP_TIME_SECONDS}
+done
 popd
 
 # Deactivate the environment cause we are done with the python steps
 conda deactivate
 
-# Go to the OpenAI logdir's parent and zip the results
-pushd ${OPENAI_LOGDIR}/..
-dir_name=`basename ${OPENAI_LOGDIR}`
+# Copy the db of experiments and the actions_info
+cp ${OPENAI_BASELINES_PATH}/workspace/db_experiments.csv ${EXPERIMENT_DIR}/.
+cp ${OPENAI_BASELINES_PATH}/workspace/actions_info.csv ${EXPERIMENT_DIR}/.
+
+# Go to the Experiments' parent directory and zip the results
+pushd ${EXPERIMENT_DIR}/..
+dir_name=`basename ${EXPERIMENT_DIR}`
+echo "Zipping the experiments ..."
 zip -qr ${dir_name}.zip ${dir_name}
 popd
