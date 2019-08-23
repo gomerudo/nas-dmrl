022531849fac32995a9514afefefc536cc091f79
Jorge Gomez <gomerudo@gmail.com>
2019-08-23 18:44:10 +0200

    Introduce Random Search script and modified notebooks

---
+++ scripts/experiments/random_search.py
@@ -0,0 +1,90 @@
+import os
+from datetime import datetime
+from random import seed
+from random import random
+from random import randint
+import argparse
+
+import gym
+import nasgym
+import pandas as pd
+
+def do_random_search(random_seed=1024, log_dir="workspace_rs_mdn10", 
+                     ntimesteps=8000):
+    env = gym.make("NAS_cifar10-v1")
+
+    seed(random_seed)
+
+    timestamp = datetime.now().strftime('%Y%m%d%H%M%S')
+    print("Starting at:", timestamp)
+
+    episode_log_dir = "{dir}/logs-{ts}".format(
+        dir=log_dir,
+        ts=timestamp
+    )
+    os.makedirs(episode_log_dir, exist_ok=True)
+    episode_log_path = "{dir}/{name}.csv".format(
+        dir=episode_log_dir,
+        name="episode_results"
+    )
+    episode_df = None
+
+    for _ in range(ntimesteps):
+        action = randint(0, env.action_space.n)
+        obs, rew, done, info_dict = env.step(action)
+
+        if episode_df is None:
+            headers = info_dict.keys()
+            episode_df = pd.DataFrame(columns=headers)
+
+        # TODO: Check if this works
+        episode_df = episode_df.append(
+            info_dict, ignore_index=True
+        )
+
+        if done:
+            # Every time we are done, we will save the csv's
+            if hasattr(env, 'save_db_experiments'):
+                print("Saving database of experiments")
+                env.save_db_experiments()
+            if episode_df is not None:
+                outfile = open(episode_log_path, 'a')
+                print("Saving episode logs")
+                episode_df.to_csv(outfile)
+                outfile.close()
+                episode_df = None
+
+            env.reset()
+
+
+    # Save at the end too
+    if hasattr(env, 'save_db_experiments'):
+        print("Saving database of experiments")
+        env.save_db_experiments()
+
+    if episode_df is not None:
+        outfile = open(episode_log_path, 'a')
+        print("Saving episode logs")
+        episode_df.to_csv(outfile)
+        outfile.close()
+        episode_df = None
+
+if __name__ == '__main__':
+    # Define the arguments
+
+    parser = argparse.ArgumentParser(
+        description='Run a random search for NAS'
+    )
+    parser.add_argument('--log_dir', action="store", dest="log_dir")
+    parser.add_argument('--ntimesteps', action="store", dest="ntimesteps")
+    parser.add_argument('--random_seed', action="store", dest="random_seed")
+
+    # Obtain the arguments of interest
+    cmd_args = parser.parse_args()
+    log_dir = cmd_args.log_dir if cmd_args.log_dir is not None else "workspace_rs_mdn10"
+    ntimesteps = cmd_args.ntimesteps if cmd_args.ntimesteps is not None else 8000
+    random_seed = cmd_args.random_seed if cmd_args.random_seed is not None else 1024
+
+    print("Log dir is", log_dir)
+    print("Ntimesteps", ntimesteps)
+    print("random_seed", random_seed)
--- scripts/experiments/run_nas_random.sh
+++ scripts/experiments/run_nas_random.sh
@@ -7,7 +7,7 @@
 usage() {
     echo "Usage:"
     echo ""
-    echo "     run_nas_dmrl.sh -c CONFIG_FILE [-m PREWARM_MODEL] [-r]"
+    echo "     run_nas_random.sh -c CONFIG_FILE [-r]"
     echo ""
 }
 
@@ -16,32 +16,21 @@ parse_var() {
 }
 
 parse_ini_file() {
-    RL_ALGORITHM="$(parse_var Algorithm)"
-    RL_ENVIRONMENT="$(parse_var Environment)"
-    RL_NETWORK="$(parse_var Network)"
-    FINAL_MODEL_NAME="$(parse_var FinalModelName)"
-    N_TASKS="$(parse_var NTasks)"
     N_TIMESTEPS="$(parse_var NumTimesteps)"
-    N_TRIALS="$(parse_var NTrials)"
+    RANDOM_SEED="$(parse_var RandomSeedGlobal)"
     GPU_MONITOR_SECONDS="$(parse_var GPUMonitorSec)"
-    SLEEP_TIME_SECONDS="$(parse_var SleepTimeSec)"
     CONFIG_LOG_PATH="$(parse_var LogPath)"
-    LOG_INTERVAL="$(parse_var LogInterval)"
-    BUFFER_SIZE="$(parse_var BufferSize)"
-    EPSILON="$(parse_var QEpsilon)"
 }
 
 ################################################################################
 ################################ PARSE OPTIONS  ################################
 ################################################################################
 
-while getopts ":c:m:r" opt; do
+while getopts ":c:r" opt; do
   case ${opt} in
     c )
         INI_FILE=$OPTARG
         ;;
-    m )
-        WARMUP_MODEL=$OPTARG
         ;;
     r )
         REMOVE=YES
@@ -72,10 +61,6 @@ fi
 
 echo "Experiment will use the configuration in file ${INI_FILE}"
 export NAS_DMRL_CONFIG_FILE=${INI_FILE}
-
-if [ ! -z ${WARMUP_MODEL} ]; then
-    echo "Experiment will use model ${WARMUP_MODEL} as a pre-warm model."
-fi
 
 ################################################################################
 ################################ GET MASTER PID  ################################
@@ -130,8 +115,6 @@ fi
 # Copy the configuration file so that we can know what we config we used
 cp ${INI_FILE} ${EXPERIMENT_DIR}/.
 
-echo "A total of ${N_TRIALS} trials will be executed"
-
 # Let the nvidia monitoring running in the background for as long as the process runs
 echo "Running GPU monitoring tool in the background. It will log every ${GPU_MONITOR_SECONDS} seconds"
 nvidia-smi \
@@ -148,65 +131,18 @@ fi
 
 export LIMITED_STORAGE=YES
 
-for trial in $(seq 1  1 ${N_TRIALS}); do
-    echo "Starting trial ${trial}"
+command="time ${CURRENT_SCRIPT_DIR}/random_search.py \
+--log_dir==${EXPERIMENT_DIR} \
+--ntimesteps=10=${N_TIMESTEPS} \
+--random_seed=${RANDOM_SEED}"
 
-    # We sleep for few seconds to let the memory release and the environment stabilize
-    # Each trial will have its one timestamp to isolate the subexperiments
-    TRIAL_TIMESTAMP=`date +%Y%m%d%H%M%S`
+echo "Command to execute is: ${command}"
+${command}
 
-    # We obtain the last OPENAI_LOGDIR in the environmet, to use it as the
-    # warmup model.
-    LAST_OPENAI_LOGDIR="${OPENAI_LOGDIR}"
+if [ ! -z ${REMOVE} ]; then
+    rm -rf ${CONFIG_LOG_PATH}/trainer*
+fi
 
-    # Export the OPENAI_LOGDIR as needed by openai-baselines project
-    LOGDIR="${EXPERIMENT_DIR}/openai-${TRIAL_TIMESTAMP}"
-    export OPENAI_LOGDIR=${LOGDIR}
-
-    # Define the place where we will store the resulting model
-    SAVE_DIR=${OPENAI_LOGDIR}/models
-
-    if [ ! -d ${SAVE_DIR} ]; then
-        mkdir -p ${SAVE_DIR}
-    fi
-
-# python -m baselines.run --alg=deepq --env=PongNoFrameskip-v4 --network=mlp --num_timesteps=1e5
-    command="time python -m baselines.run \
---alg=${RL_ALGORITHM} \
---env=${RL_ENVIRONMENT} \
---seed=1024 \
---network=${RL_NETWORK} \
---save_path=${SAVE_DIR}/${FINAL_MODEL_NAME} \
---buffer_size=${BUFFER_SIZE} \
---exploration_fraction=1.0 \
---exploration_final_eps=${EPSILON} \
---train_freq=1 \
---batch_size=20 \
---checkpoint_freq=100 \
---gamma=1.0 \
---target_network_update_freq=100 \
---num_timesteps=${N_TIMESTEPS}"
-
-    if [ ${trial} -eq 1 ]; then
-        if [ ! -z ${WARMUP_MODEL} ]; then
-            command="${command} --load_path=${WARMUP_MODEL}"
-        fi
-    else
-        if [ ! -z ${LAST_OPENAI_LOGDIR} ]; then
-            command="${command} --load_path=${LAST_OPENAI_LOGDIR}/models/${FINAL_MODEL_NAME}"
-        fi
-    fi
-
-    echo "Command to execute is: ${command}"
-    ${command}
-
-    if [ ! -z ${REMOVE} ]; then
-        rm -rf ${CONFIG_LOG_PATH}/trainer*
-    fi
-
-    # Always wait after the command has been executed
-    sleep ${SLEEP_TIME_SECONDS}
-done
 popd
 
 # Copy the db of experiments and the actions_info
